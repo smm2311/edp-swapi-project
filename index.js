57f8683ebd478c2f153fecb8166fc1da78f90d42
Chikwendu Mgbechi <chikwe2002@gmail.com>
2025-06-24 09:59:41 -0400

    added planets to home page

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const filmsList = document.querySelector("#filmsList")
 // Is this necessary?
 document.addEventListener('DOMContentLoaded', getFilms)
 
+let planets = [];
+let matchingPlanets = [];
+const planetsList = document.querySelector("#planetsList")
+
+// Is this necessary?
+document.addEventListener('DOMContentLoaded', getPlanets)
+
 async function getCharacters() {
   let url = 'http://localhost:9001/api/characters';
 
@@ -78,3 +85,37 @@ const renderFilms = films => {
 }
 
 const goToFilmPage = id => window.location = `/film.html?id=${id}`
+
+async function getPlanets() {
+  let url = 'http://localhost:9001/api/planets';
+
+  try {
+    const fetchedPlanets = await fetch(url)
+      .then(res => res.json())
+      planets.push(...fetchedPlanets);
+  }
+  catch (ex) {
+    console.error("Error reading planets.", ex.message);
+  }
+  console.log("All the planets are ", planets)
+  renderPlanets(planets);
+}
+
+const filterPlanets = () => {
+  const planetSearchString = document.querySelector("#planetsSearchString").value;
+  const re = new RegExp(planetSearchString, "i");
+  matchingPlanets = planets.filter(planet => re.test(planet.name))
+  renderPlanets(matchingPlanets);
+}
+
+const renderPlanets = planets => {
+  const divs = planets.map(planet => {
+    const el = document.createElement('div');
+    el.addEventListener('click', () => goToPlanetPage(planet.id));
+    el.textContent = planet.name;
+    return el;
+  })
+  planetsList.replaceChildren(...divs)
+}
+
+const goToPlanetPage = id => window.location = `/planet.html?id=${id}`
